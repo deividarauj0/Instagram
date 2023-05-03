5dc0b9e288ba6979e6009bb2f9851e634db2ce47
deividarauj0 <deivid-ns@hotmail.com>
2023-05-03 14:29:41 -0300

    Aula_01

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,39 @@
+class PostsController < ApplicationController
+  before_action :set_post, only: %i[ show ]
+
+  # GET /posts or /posts.json
+  def index
+    @posts = Post.all
+  end
+
+  # GET /posts/1 or /posts/1.json
+  def show
+  end
+
+  # GET /posts/new
+  def new
+    @post = Post.new
+  end
+
+  # POST /posts or /posts.json
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      redirect_to post_url(@post), notice: "Post criado com sucesso."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_post
+      @post = Post.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def post_params
+      params.require(:post).permit(:description)
+    end
+end
