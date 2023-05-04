d805271d0e13982168d7f3f6027e4a522e832a63
deividarauj0 <deivid-ns@hotmail.com>
2023-05-04 20:56:32 -0300

    Aula_06

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
   root "posts#index"
+
+  devise_for :users, only: %i[sessions registrations passwords]
 
   resources :posts, only: %i[index show new create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
